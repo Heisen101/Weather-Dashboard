2792e16124e341be2c3e7830c369a81425f4e23d
Vasile <vasileholding@gmail.com>
2023-08-11 17:36:20 +0100

    added plan for starting js code

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,11 @@ var inputSearch = $("#search-input");
 var searchForm = $("#search-form");
 var todayArea = $("#today");
 var forecastFive = $("#forecast");
+
+//code for API+ url + search
+
+//function for buttons to search arrea and created buttons saved in local storage
+
+//function for displaing todays wheather
+
+//function for displaying five days forecast wheather and ellemnts creat
