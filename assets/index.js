3f881a24e578a445741538e8878994ad75603720
Vasile <vasileholding@gmail.com>
2023-08-11 16:55:13 +0100

    added variables in js

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+var buttonHistory = $("#history");
+var searchButton = $("#search-button");
+var inputSearch = $("#search-input");
+var searchForm = $("#search-form");
+var todayArea = $("#today");
+var forecastFive = $("#forecast");
