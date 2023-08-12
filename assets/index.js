c731fc31950dda216c1af3514e2b9953fd04206e
Vasile <vasileholding@gmail.com>
2023-08-12 13:12:15 +0100

    added comments , added a new function for taking the input from search area and be used in function , adding function to create search history buttons

--- assets/index.js
+++ assets/index.js
@@ -8,13 +8,36 @@ var apiKey = "d312684dbe502497c563af0a35330883";
 var units = "metric";
 var limit = 5;
 //code for API+ url + search +geolocation
-function LongLat() {
+function LongLat(CityName) {
   // will show the city wheather
-  let coordinatesUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputSearch.val()}&limit=${limit}&appid=${apiKey}`;
+  let coordinatesUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${CityName}&limit=5&appid=${apiKey}`;
   console.log(coordinatesUrl);
+  fetch(coordinatesUrl)
+    .then((response) => response.json())
+    .then((coorditanates) => {
+      if (coorditanates.cod === "404") {
+        alert("Location not found. Please enter a different city name.");
+        return;
+      }
+      localStorage.setItem(CityName, JSON.stringify(coorditanates));
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 }
-LongLat();
+//this will take the value from input area and put inside funcion parameter when search button pressed
+$("#search-form").submit(function (event) {
+  event.preventDefault(); //prevents de default behavior of form
+  var CityName = inputSearch.val();
+  LongLat(CityName);
+});
+
 //function for buttons to search arrea and created buttons saved in local storage
+
+function searchHistory() {
+  var button = $("<button>").addClass("btn btn-secondary").css;
+  var div = $("<div>").addClass("card mx-3 my-3").css({});
+}
 
 //function for displaing todays wheather
 
