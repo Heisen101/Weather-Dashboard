2b28fac2f403fbb4a5db478e146ae12f662aade1
Vasile <vasileholding@gmail.com>
2023-08-12 11:55:15 +0100

    added API

--- assets/index.js
+++ assets/index.js
@@ -4,9 +4,16 @@ var inputSearch = $("#search-input");
 var searchForm = $("#search-form");
 var todayArea = $("#today");
 var forecastFive = $("#forecast");
-
-//code for API+ url + search
-
+var apiKey = "d312684dbe502497c563af0a35330883";
+var units = "metric";
+var limit = 5;
+//code for API+ url + search +geolocation
+function LongLat() {
+  // will show the city wheather
+  let coordinatesUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputSearch.val()}&limit=${limit}&appid=${apiKey}`;
+  console.log(coordinatesUrl);
+}
+LongLat();
 //function for buttons to search arrea and created buttons saved in local storage
 
 //function for displaing todays wheather
